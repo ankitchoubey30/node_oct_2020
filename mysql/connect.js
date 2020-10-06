b0d5e80735b2aa06548219a87154d0a0b7310461
prafful <prraful@gmail.com>
2020-10-05 23:33:57 -0700

    express - get

--- mysql/connect.js
+++ mysql/connect.js
@@ -14,17 +14,36 @@ connection.connect(function(error){
         throw error
     }
     console.log("Connected to MySQL Database!!!!");
-    var sql_create_table = "create table friends (id int(8), name varchar(32), location varchar(32))"
-    connection.query(sql_create_table, function(error, success){
+    //var sql_create_table = "create table friends (id int(8), name varchar(32), location varchar(32))"
+    //var sql_insert_values = "insert into friends (id, name, location) values (4, 'cas','chennai')) "
+    var sql_insert_values = "insert into friends (id, name, location) values ? "
+    var multiple_values = [
+        [1, 'obb', 'chennai'],
+        [2, 'bnp', 'ahmedabad'],
+        [3, 'owioh', 'japan'],
+        [4, 'mlann', 'bengaluru']
+    ]
+    connection.query(sql_insert_values, [multiple_values] ,function(error, success){
         if(error){
             throw error
         }
-        console.log("Created table - friends!!!!");
+        //console.log("Created table - friends!!!!");
+        console.log("Multiple values insert success in friends!!!!");
+    })
+
+
+    var select_query = "select id,name, location from friends"
+    connection.query(select_query, function(error, success){
+        if(error){
+            throw error
+        }
+        //console.log("Created table - friends!!!!");
+        console.log("Rows received from table!!!!");
+        console.log(success);
+        console.log(JSON.stringify(success));
     })
 
 })
-
-
 /* connection.connect(function(error){
     if(error){
         throw new Error("Not able to connect to server!!!!")
--- rest-api/index.js
+++ rest-api/index.js
@@ -1,5 +1,23 @@
 var express = require('express')
 var bodyparser = require('body-parser')
+var mysql = require("mysql")
+
+
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    port:3306,
+    database:'socialfeed'
+})
+
+//use conenction object to connect to database
+connection.connect(function(error){
+    if(error){
+        throw error
+    }
+    console.log("Connected to database - socialfeed");
+})
 
 
 var app = express()
@@ -11,8 +29,16 @@ app.get('/', function(request, response){
     response.send('Hello from node and express!!!!')
 })
 
-app.get('/all', function(request, response){
-    response.send('Get list of all friends!!!!')
+app.get('/friends/all', function(request, response){
+    var query = "select * from friends"
+    connection.query(query, function(err, success){
+        if(err){
+            throw err
+        } 
+        console.log(success);
+        response.json(success)
+    })
+   
 })
 
 app.post('/add', function(request, response){
