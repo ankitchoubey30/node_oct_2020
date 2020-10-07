7be35048698b98887ad7893e693cd20c2425ef31
prafful <prraful@gmail.com>
2020-10-07 00:24:34 -0700

    rest client - axios with express

--- 005fileio.js
+++ 005fileio.js
@@ -1,12 +1,57 @@
 var fs = require('fs')
 
 //console.log(fs)
+console.log("File read will start");
 
-fs.readFile("myfile.txt", function(err, data){
+try {
+    fs.readFileSync("myfile.txt", function(err, data){
+        console.log("File read started!");
+        if (err){
+            throw err 
+        }
+        console.log(data)
+        console.log(data.toString() )
+        console.log("File read in progress");
+    })
+    
+} catch (err) {
+    console.log("In catch block file");
+    console.log(err);
+}
+
+console.log("File read is done");
+
+console.log("File2 read will start");
+
+try {
+    fs.readFileSync("myfile2.txt", function(err, data){
+        console.log("File2 read started!");
+        if (err){
+            throw err 
+        }
+        console.log(data)
+        console.log(data.toString() )
+        console.log("File2 read in progress");
+    })
+    
+} catch (err) {
+    console.log("In catch block file2");
+    console.log(err);
+}
+
+console.log("File2 read is done");
+
+/* //console.log(fs)
+console.log("File2 read will start");
+fs.readFile("myfile2.txt", function(err, data){
+    console.log("File2 read started!");
     if (err){
         throw err 
     }
     console.log(data)
     console.log(data.toString() )
+    console.log("File2 read in progress");
 })
 
+console.log("File2 read is done");
+ */
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var ejs = require('ejs')  //this is optional
 var https = require('https')
+var axios = require('axios')
 
 //initialize the express app
 var app = express()
@@ -22,8 +23,8 @@ app.get('/contact/:name', function(req, res){
 //serve static pages from public folder!
 app.use(express.static('public'))
 
-//use rest client to communicate with external middlewares
-app.get('/remote/users/', function(req, res){
+//use https rest client to communicate with external middlewares
+app.get('/https/users/', function(req, res){
     console.log("Get data from remote rest api!!!!")
     https.get('https://jsonplaceholder.typicode.com/users/' , function(response){
 
@@ -38,11 +39,96 @@ app.get('/remote/users/', function(req, res){
            res.json(JSON.parse(data))
         })
 
+    })
+})
 
+app.get('/https/users/:id', function(req, res){
+    console.log("Get data from remote rest api!!!!")
+    https.get('https://jsonplaceholder.typicode.com/users/' + req.params.id , function(response){
+
+        let data = ''      
+
+        response.on('data', function(dataPacket){
+           data += dataPacket
+        })   
+
+        response.on('end', function(){
+           console.log(JSON.parse(data)) 
+           let object = JSON.parse(data)
+           console.log(object.name)
+           console.log(object.email)
+           console.log(object.address.geo.lat)
+           console.log(object.address.geo.lng)
+           res.json(JSON.parse(data))
+        })
+
+    })
+})
+
+//not recommended
+app.get('/https/donotdo/users/:id', function(req, res){
+    console.log("Get data from remote rest api!!!!")
+    https.get('https://jsonplaceholder.typicode.com/users/', function(response){
+
+        let data = ''      
+
+        response.on('data', function(dataPacket){
+           data += dataPacket
+        })   
+
+        response.on('end', function(){
+           console.log(JSON.parse(data)) 
+           let allObjects = JSON.parse(data)
+           res.json(allObjects[req.params.id-1])
+        })
 
     })
 })
 
 
+app.get('/https/template/users/:id', function(req, res){
+    console.log("Get data from remote rest api!!!!")
+    https.get('https://jsonplaceholder.typicode.com/users/' + req.params.id , function(response){
+
+        let data = ''      
+
+        response.on('data', function(dataPacket){
+           data += dataPacket
+        })   
+
+        response.on('end', function(){
+           console.log(JSON.parse(data)) 
+           let object = JSON.parse(data)
+           console.log(object.name)
+           console.log(object.email)
+           console.log(object.address.geo.lat)
+           console.log(object.address.geo.lng)
+           res.render('user', { 
+                                name: object.name, 
+                                username:object.username,
+                                email: object.email,
+                                latitude: object.address.geo.lat,
+                                longitude: object.address.geo.lng
+                            })
+        })
+
+    })
+})
+
+
+//use axios rest client to communicate with external middlewares
+app.get('/axios/users/', function(req, res){
+    console.log("Get data from remote rest api!!!!")
+    axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(function(response){
+            console.log(response.data)
+            res.json(response.data)
+        })
+        .catch(function(error){
+            console.log(error)
+            res.json({message: "error"})
+        })
+
+})
 
 app.listen(8080)
